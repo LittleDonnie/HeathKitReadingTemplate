a9c2dbf1776b4d7480b8cc2c5272beb96080deef
Monty Montgomery <poweredbyplants@gmail.com>
2023-02-15 13:32:47 -0600

    Initial Commit

---
+++ HealthKit Try One/HealthKit_Try_OneApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKit_Try_OneApp.swift
+//  HealthKit Try One
+//
+//  Created by Donald Montgomery on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct HealthKit_Try_OneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
